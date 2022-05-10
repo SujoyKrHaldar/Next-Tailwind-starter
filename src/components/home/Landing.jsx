2b4cc9f7d055f4cf72372b1581fa1ebbc178890c
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2022-05-10 21:58:15 +0530

    first commit

---
+++ src/components/home/Landing.jsx
@@ -0,0 +1,16 @@
+function Landing() {
+  return (
+    <>
+      <div className="w-full h-[90vh]">
+        <div className="container w-full h-full flex items-center justify-start">
+          <div className="max-w-lg py-4">
+            <p className="mt-4 text-xl">Next tailwind starter</p>
+            <h1>Let's make the work flow easy and usefull.</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Landing;
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <>
+      <div className="bg-[#1a1a1a] text-[#929292] py-6  mt-auto">
+        <div className="container">
+          <p>Copyright ©{new Date().getFullYear()} - All rights reserved.</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
